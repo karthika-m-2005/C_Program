1f77e710841414e36c63580380ea70574776bcae
KARTHIKA M <km646383@gmail.com>
2025-05-31 17:56:47 +0530

    Datatype Based C Program

--- Datatype_Based_Program.c
+++ Datatype_Based_Program.c
@@ -1,1 +1,26 @@
 
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    unsigned int b;
+    float c;
+    char d;
+    unsigned char e;
+    double f;
+    long double g;
+    short int h;
+    unsigned short int i;
+    long int j;
+    unsigned long int k;
+    long long int l;
+    unsigned long long int m;
+
+    printf("Enter integer unsignedinteger float char unsignedchar double longdouble shortint unsignedshortint longint unsignedlongint longlongint unsignedlonglongint : ");
+    scanf("%d %u %f %c %c %lf %Lf %hd %hu %ld %lu %lld %llu",&a, &b, &c, " %c", " %c", &f, &g, &h, &i, &j, &k, &l, &m);
+
+    printf("%d %u %f %c %c %lf %Lf %hd %hu %ld %lu %lld %llu\n", a, b, c, d, e, f, g, h, i, j, k, l, m);
+
+    return 0;
+}
