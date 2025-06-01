b0b1da1992b0fea0126bcff02dea1f1c697d53ee
KARTHIKA M <km646383@gmail.com>
2025-06-01 20:40:25 +0530

    Vowel Program in C

---
+++ Vowel_Program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  char input;
+  printf("Enter a Vowel: ");
+  scanf("%c",&input);
+  switch()
+  {
+  case 'a':
+  case 'e':
+  case 'i': 
+  case 'o':
+  case 'u': 
+      printf("Vowel");
+      break;
+default:
+     printf("Consonent");
+  }
+}
+  
