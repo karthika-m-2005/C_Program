cd9e63fdba968d5d24d471db122c22f24085b9bb
KARTHIKA M <km646383@gmail.com>
2025-06-14 21:54:19 +0530

    Update and rename Largest_Number.C to Largest_Number.c

---
+++ Largest_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the a value:");
+scanf("%d",&a);
+printf("Enter the b value:");
+scanf("%d",&b);
+printf("Enter the c value:");
+scanf("%d",&c);
+if(a>b) && a>c
+{
+printf("a is Largest Number");
+}
+else if(b>a && b>c)
+{
+printf("b is Largest Number");
+}
+else
+{
+printf("c is the Largest Number");
+}
+}
+
